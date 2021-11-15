56f9808235113d6801c99cb58e7c66dca9ac61ad
Ilyasse Idkhebbach <iidkhebb@e1r10p5.1337.ma>
2021-11-16 00:43:01 +0100

    added some shortcuts

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -20,29 +20,10 @@ int	buffer_len(char *str_static)
 		return (-1);
 	i = 0;
 	while (str_static[i] != '\0' && str_static[i] != '\n')
-	{
 		i++;
-	}
 	if (str_static[i] == '\0')
 		return (-1);
 	return (i);
-}
-
-char	*next_line_maker(char *str_static, int pos)
-{
-	int		i;
-	char	*new_str;
-	int		line_len;
-
-	i = 0;
-	line_len = buffer_len(str_static);
-	new_str = (char *)malloc((pos + 2) * sizeof(char));
-	if (!new_str)
-		return (0);
-	ft_memmove(new_str, str_static, line_len);
-	new_str[line_len] = '\n';
-	new_str[line_len + 1] = '\0';
-	return (new_str);
 }
 
 char	*line_cutter(char *str_static)
@@ -50,15 +31,17 @@ char	*line_cutter(char *str_static)
 	char	*new_str;
 	int		pos;
 
-	if (ft_strlen(str_static) <= 0)
-		return (0);
+	if (!str_static)
+		return (NULL);
 	pos = buffer_len(str_static);
-	if (pos == -1 || pos == (int)ft_strlen(str_static) - 1)
-	{
-		new_str = ft_strdup(str_static);
-		return (new_str);
-	}
-	new_str = next_line_maker(str_static, pos);
+	if (pos == -1)
+		return (ft_strdup(str_static));
+	new_str = (char *)malloc((pos + 2) * sizeof(char));
+	if (!new_str)
+		return (NULL);
+	ft_memmove(new_str, str_static, pos);
+	new_str[pos] = '\n';
+	new_str[pos + 1] = '\0';
 	return (new_str);
 }
 
@@ -68,43 +51,38 @@ char	*buffer_old_line_rm(char *str_static)
 	int	pos;
 
 	if (!str_static)
-		return (0);
+		return (NULL);
 	remaining_len = (int)ft_strlen(str_static);
 	pos = buffer_len(str_static);
 	if (pos == -1 || ((remaining_len - pos) == 1))
-	{
-		free(str_static);
-		return (0);
-	}
+		return (free(str_static), NULL);
 	str_static = ft_substr(str_static, (pos + 1), (remaining_len - pos - 1));
 	return (str_static);
 }
 
-char	*get_next_line(int fd_track)
+char	*get_next_line(int fd)
 {
 	char		*str_tmp;
-	static char	*str_static[10000000];
-	int			read_return_index;
+	static char	*str_static[10000];
+	int			rtr;
 	char		*new_str;
 
-	if (fd_track < 0 || BUFFER_SIZE <= 0)
-		return (0);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	str_tmp = malloc(BUFFER_SIZE + 1);
-	if (!(str_tmp))
-		return (0);
-	while (buffer_len(str_static[fd_track]) == -1)
+	if (!str_tmp)
+		return (NULL);
+	while (buffer_len(str_static[fd]) == -1)
 	{
-		read_return_index = read(fd_track, str_tmp, BUFFER_SIZE);
-		if (read_return_index <= 0)
+		rtr = read(fd, str_tmp, BUFFER_SIZE);
+		if (rtr <= 0)
 			break ;
-		str_tmp[read_return_index] = '\0';
-		str_static[fd_track] = ft_strjoin(str_static[fd_track], str_tmp);
+		str_tmp[rtr] = '\0';
+		str_static[fd] = ft_strjoin(str_static[fd], str_tmp);
 	}
 	free(str_tmp);
-	if (read_return_index == -1)
-		return (0);
-	new_str = line_cutter(str_static[fd_track]);
-	str_static[fd_track] = buffer_old_line_rm(str_static[fd_track]);
+	new_str = line_cutter(str_static[fd]);
+	str_static[fd] = buffer_old_line_rm(str_static[fd]);
 	return (new_str);
 }
 /*
